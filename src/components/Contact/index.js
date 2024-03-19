c4607de6ff2d36dba20171ce11aebccee7c000bd
PaulCrawfordOrtex <paul.crawford@ortex.com>
2024-03-19 00:24:17 +0000

    init

---
+++ src/components/Contact/index.js
@@ -0,0 +1,87 @@
+import Loader from 'react-loaders'
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useRef, useState } from 'react';
+import emailjs from '@emailjs/browser'
+
+const Contact = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const refForm = useRef()
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000);
+        
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [])
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs
+        .sendForm(
+            'service_04uitml',
+            'template_qk2n89r',
+            refForm.current, {
+               publicKey: 'GrNlT0EYKaj6IHr68', 
+            }
+                       
+        )
+        .then(
+            () => {
+              alert('Message has been sent!')
+              window.location.reload(false)
+            },
+            () => {
+                alert('Failed to send message please try again!')
+            },
+          );
+      };
+
+    return (
+        <>
+        <div className='container contact-page'>
+        <div className='text-zone'>
+            <h1>
+                <AnimatedLetters
+                letterClass={letterClass} strArray={['C','o','n','t','a','c','t',' ', 'm', 'e']} 
+                idx={15}
+                />
+            </h1>
+            <p>
+            I am interested in freelance / permanent and contract opportunities - especially on ambitious
+            or large projects. However, if you have any other requests or
+            questions, don't hesitate to contact me using below form either.
+            </p>
+            <div className='contact-form'>
+                <form ref={refForm} onSubmit={sendEmail}>
+                    <ul>
+                       <li className="half">
+                        <input type="text" name="name" placeholder="Name" required />
+                        </li>
+                        <li className="half">
+                        <input type="email" name="email" placeholder="Email" required />
+                        </li>
+                        <li>
+                            <input placeholder="Subject" type="text" name="subject" required />
+                        </li>
+                        <li>
+                            <textarea placeholder="Message" name="message" required></textarea>
+                        </li>
+                        <li>
+                            <input type="submit" className='flat-button' value="SEND" />
+                            </li>  
+                    </ul>
+                </form>
+            </div>
+        </div>
+        </div>
+        <Loader type="pacman" />
+        </>
+    )
+}
+
+export default Contact
---
+++ src/components/Home/index.js
@@ -0,0 +1,54 @@
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-s.png'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import { useEffect, useState } from 'react'
+import Logo from './Logo'
+import Loader from 'react-loaders'
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['a','u','l']
+    const jobArray = ['a', ' ','w','e','b','','d','e','v','e','l','o','p','e','r','.']
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 4000);
+        
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, []);
+
+    return (
+        <>
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>
+                    <span className={letterClass}>H </span>
+                    <span className={`${letterClass} _12`}>i, </span>
+                    <br/>
+                    <span className={`${letterClass} _13`}>I </span>
+                    <span className={`${letterClass} _14`}>'m </span>
+                <img src={LogoTitle} alt="developer" />
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={15} />
+                <br/>
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                idx={20} />
+                </h1>
+                <h2>Frontend Developer / Javascript Wiz / Tekken & Super Smash bros master</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            </div>
+            <Logo />
+
+        </div>
+        <Loader type="pacman" />
+        </>
+    )
+}
+
+export default Home
